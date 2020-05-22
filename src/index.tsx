fadf50ef3ace46494bca811ab284af31d604fb94
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-05-23 01:57:43 +0530

    React functional components

--- src/index.tsx
+++ src/index.tsx
@@ -9,30 +9,34 @@ interface AppProps {
 //   counter: number;
 // }
 
-class App extends React.Component<AppProps> {
-  // constructor(props: AppProps) {
-  //   super(props);
-  //   this.state = { counter: 0 };
-  // }
-  state = { counter: 0 };
+const App = (props: AppProps): JSX.Element => {
+  return <div>Hello Gugu</div>;
+};
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+// class App extends React.Component<AppProps> {
+//   // constructor(props: AppProps) {
+//   //   super(props);
+//   //   this.state = { counter: 0 };
+//   // }
+//   state = { counter: 0 };
+//
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+//
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+//
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
